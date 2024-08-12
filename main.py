53f7d94d280fc67f193c9927801651678a5ed702
马云龙 <mayunlong@ceshiren.com>
2024-08-12 10:14:54 +0800

    1

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from datetime import datetime
 
 # 获取当前时间
 current_time = datetime.now()
-yaml.safe_dump([current_time], open('data/config.yaml', encoding='utf-8'))
+yaml.safe_dump([current_time], open('data/config.yaml','w', encoding='utf-8'))
